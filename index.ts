de63b4e80706d3598650264e87e2362f1907cad9
Aman07a <aman16.aa17@gmail.com>
2024-01-26 13:45:10 +0100

    Introducing Interfaces

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
+interface PostGatekeeper {
+  title: string;
+  daysOld: number;
+  published: boolean;
+}
+
 const post = {
   title: 'Latest Typescript News',
   daysOld: 10,
   published: true,
 };
 
-const printPost = (postToPrint: {
-  title: string;
-  daysOld: number;
-  published: boolean;
-}) => {
+const printPost = (postToPrint: PostGatekeeper) => {
   return `${postToPrint.title} (${postToPrint.daysOld} days old)`;
 };
+
+printPost(post);
