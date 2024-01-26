6cfd11d9a38d0ad47eea5b03c39723cf0dd575b5
Aman07a <aman16.aa17@gmail.com>
2024-01-26 09:30:08 +0100

    Type Inference

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log('Hi there');
+const myName: any = undefined;
+
+const ten: number = 10;
+const negative: number = -10000;
+const decimal: number = 10.00000001;
+
+const yes: boolean = true;
+
+const nothingHere: null = null;
+const noDefinition: undefined = undefined;
